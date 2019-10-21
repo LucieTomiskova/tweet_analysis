885c5a8d115e3ba14de880ba9ff1ea8e33f48bf5
Lucie Tomiskova <tomiskovalucie@gmail.com>
2019-10-21 23:43:41 +0200

    added corr

--- core/sentiment_visualizations.py
+++ core/sentiment_visualizations.py
@@ -1,10 +1,9 @@
 import json
 import matplotlib.pyplot as plt
-import matplotlib.dates as mdates
 import pandas as pd
 from configuration.config import root
 
-SENTIMENT_PATH = root / 'core' / 'output_sentiment.json'
+SENTIMENT_PATH = root / 'data' / 'sentiment_avg_daily.json'
 BITCOIN_PRICE_PATH = root / 'data' / 'bitcoin_price.json'
 
 
@@ -16,18 +15,26 @@ def read_json(json_path):
 
 def create_df(json_text, index_column):
     df = pd.DataFrame(json_text)
-    df[index_column] = pd.to_datetime(df[index_column])
+    df[index_column] = pd.to_datetime(df[index_column], format='%Y-%m-%d %H:%M:%S')
     df = df.set_index(index_column)
+    df.index = df.index.strftime('%Y-%m-%d')
+    df = df.sort_index()
     return df
 
 
 def get_lineplot(df, column_name, title, ylabel, xlabel):
-    plot_ts = df[column_name].plot(linewidth=0.5)
+    df[column_name].plot(linewidth=0.5)
     plt.title(title, fontsize=14)
     plt.ylabel(ylabel, fontsize=10)
     plt.xlabel(xlabel, fontsize=10)
-    xfmt = mdates.DateFormatter('%y-%m-%d')
-    plot_ts.xaxis.set_major_formatter(xfmt)
+
+
+def get_scatterplot(x, y, title, ylabel, xlabel):
+    plt.scatter(x, y)
+    plt.title(title, fontsize=14)
+    plt.ylabel(ylabel, fontsize=10)
+    plt.xlabel(xlabel, fontsize=10)
+    plt.show()
 
 
 if __name__ == '__main__':
@@ -41,11 +48,14 @@ if __name__ == '__main__':
     bitcoin_price_json = read_json(BITCOIN_PRICE_PATH)
     bitcoin_price = {'date': list(bitcoin_price_json.keys()), 'bitcoin_price': list(bitcoin_price_json.values())}
     bitcoin_price_df = create_df(bitcoin_price, 'date')
-    print(bitcoin_price_df)
 
     plt.subplot(212)
     get_lineplot(bitcoin_price_df, 'bitcoin_price', 'Vývoj ceny Bitcoinu', 'cena', 'datum')
 
     plt.tight_layout()
     plt.show()
+
+    merged_dfs = pd.merge(tweets_df, bitcoin_price_df, left_index=True, right_index=True)
+
+    get_scatterplot(merged_dfs['avg'], merged_dfs['bitcoin_price'], 'Correlation between BTC price and sentiment', 'BTC price', 'sentiment')
 
